62926c1b29ffec5acff5c6b492f97be5d639fb41
hawkx <hawkx@163.com>
2018-12-28 11:09:35 +0800

    original code with changed package path and source file re-formatted
    Just original code with changed package path and source file re-formatted, the code actually not modified

---
+++ README.md
@@ -0,0 +1,14 @@
+# xjflac-codec
+Just a bit change from JFLAC library 1.5.2
+
+This project was copied from a forked jflac project at <https://github.com/nguillaumin/jflac>, the original project site was <http://jflac.sourceforge.net/>
+
+I need the jflac work together with java sound api, but the original project seems not be updated for a long time, so I copied this project.
+
+I copied this project just for my own purposes, but I had no knowledge about FLAC codec nor java sound api, so this project is most likely not updated, please don't depends on this project, thanks!
+
+# changes
+1.  change package path to win.zqxu.xjflac
+2.  all source code file re-formatted
+3.  change compile warning code, using Java 8 generic style
+4.  add frame length support to get music duration
---
+++ src/main/java/win/zqxu/xjflac/FrameListeners.java
@@ -0,0 +1,98 @@
+/*
+ * Created on Jun 28, 2004
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package win.zqxu.xjflac;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+import win.zqxu.xjflac.frame.Frame;
+import win.zqxu.xjflac.metadata.Metadata;
+
+/**
+ * Class to handle frame listeners.
+ * 
+ * @author kc7bfi
+ */
+class FrameListeners implements FrameListener {
+  private HashSet frameListeners = new HashSet();
+
+  /**
+   * Add a frame listener.
+   * 
+   * @param listener
+   *          The frame listener to add
+   */
+  public void addFrameListener(FrameListener listener) {
+    synchronized (frameListeners) {
+      frameListeners.add(listener);
+    }
+  }
+
+  /**
+   * Remove a frame listener.
+   * 
+   * @param listener
+   *          The frame listener to remove
+   */
+  public void removeFrameListener(FrameListener listener) {
+    synchronized (frameListeners) {
+      frameListeners.remove(listener);
+    }
+  }
+
+  /**
+   * Process metadata records.
+   * 
+   * @param metadata
+   *          the metadata block
+   * @see win.zqxu.xjflac.FrameListener#processMetadata(org.jflac.metadata.MetadataBase)
+   */
+  public void processMetadata(Metadata metadata) {
+    synchronized (frameListeners) {
+      Iterator it = frameListeners.iterator();
+      while (it.hasNext()) {
+        FrameListener listener = (FrameListener) it.next();
+        listener.processMetadata(metadata);
+      }
+    }
+  }
+
+  /**
+   * Process data frames.
+   * 
+   * @param frame
+   *          the data frame
+   * @see win.zqxu.xjflac.FrameListener#processFrame(win.zqxu.xjflac.frame.Frame)
+   */
+  public void processFrame(Frame frame) {
+    synchronized (frameListeners) {
+      Iterator it = frameListeners.iterator();
+      while (it.hasNext()) {
+        FrameListener listener = (FrameListener) it.next();
+        listener.processFrame(frame);
+      }
+    }
+  }
+
+  /**
+   * Called for each frame error detected.
+   * 
+   * @param msg
+   *          The error message
+   * @see win.zqxu.xjflac.FrameListener#processError(java.lang.String)
+   */
+  public void processError(String msg) {
+    synchronized (frameListeners) {
+      Iterator it = frameListeners.iterator();
+      while (it.hasNext()) {
+        FrameListener listener = (FrameListener) it.next();
+        listener.processError(msg);
+      }
+    }
+  }
+
+}
